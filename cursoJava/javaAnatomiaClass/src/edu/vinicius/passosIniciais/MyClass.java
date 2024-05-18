8af3aa06b7f65f829e0ed158d5e7b5b92e0c8c56
Vinicius Cunha <124714985+Sapin07@users.noreply.github.com>
2024-05-18 17:53:40 -0300

    Algoritmos iniciais com syntax java
    Codigos simples explicando a algumas boas praticas e como funciona a estrutura java

---
+++ cursoJava/javaAnatomiaClass/src/edu/vinicius/passosIniciais/MyClass.java
@@ -0,0 +1,54 @@
+package edu.vinicius.passosIniciais;
+
+public class MyClass {
+
+    //------Hello World--------//
+   /*
+    public static void main(String [] args) throws Exception{
+        System.out.println("Hello World");
+    }
+    //------Variaveis e metodos------//
+
+
+    public static void main(String [] args) {
+        String primeiroNome = "Vinicius";
+        String segundoNome = "Cunha";
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+    //---------Identacao----------//
+    //Segue o padrao chefe 👍
+
+    //-------Organizacao de arquivos--------//
+        Ao longo do desenvolvimento surgem novos arquivos na estrutura. Esses arquivos sao organizados por > Packages <.
+
+        > Convensao de Subdiretorios
+
+            *Comercial      => com
+            *Organizacional => org
+            *OpenSource     => org ou opensource
+
+            * Exemplo
+                ( nome empresa == HyperTech ){
+                    com.hypertech."nome do projeto".app/model/util/service
+                }
+                ( nome pessoal == Vinicius ) {
+                    edu.vinicius."tema"
+                }
+
+
+    //-------Java Beans--------//
+    convensoes da comunidade de java
+
+        * Nome de var     => Clara sem abreviacoes, sempre no singular ( exceto array ou colecoes ) , Idioma Unico.
+        * Nome de Metodos => Verbos 👍. Uma unica funcionalidade pra ficar nice.
+
+     */
+
+
+}
---
+++ cursoJava/javaAnatomiaClass/src/edu/vinicius/varTypes/VarTypes.java
@@ -0,0 +1,6 @@
+package edu.vinicius.varTypes;
+
+public class VarTypes {
+
+    
+}
