ebce97dfc61673004a774c39f2e4145e4fc1e56a
Sapin07 <sapinleste@outlook.com>
2024-05-18 18:35:09 -0300

    Explicacao basica dos tipos de Variavel java
    Impletacao de uma tabela para facilitacao da visualizacao das variaveis

--- cursoJava/javaAnatomiaClass/src/edu/vinicius/varTypes/VarTypes.java
+++ cursoJava/javaAnatomiaClass/src/edu/vinicius/varTypes/VarTypes.java
@@ -2,5 +2,26 @@ package edu.vinicius.varTypes;
 
 public class VarTypes {
 
-    
+    /*
+        //-------Tipos Primitivos------------//
+
+        ||~--------Numericos Inteiros-----||
+          Tipo   |   Memoria   |   Tamanho
+        * byte   |  1 byte     | pequenininho
+        * short  |  2 bytes    |  pequeno
+        * int    |  3 bytes    |  Geralzao
+        * long   |  4 bytes    |  Grande
+
+        ||-------Numericos Fracionarios----||
+         Tipo     |   Memoria   |   Tamanho
+        * float   |  4 byte     |   Normal
+        * double  |  8 bytes    |   Grande
+
+       NOTA* Double e mais indicado, Isso ocorre pois o float apenas mantem
+       precisao de 6 a 7 digitos decimais
+
+       //-----Declaracao de Var-------//
+            <tipo*><nome*> <AtribuirValor>
+
+    */
 }
